15e629673f5a3580aa5d4005f2a7fe7f17e978fd
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-01-01 10:43:28 -0600

    game over screen works

--- js-effects/screenEffects.js
+++ js-effects/screenEffects.js
@@ -2,6 +2,7 @@
 const mainscreen = document.getElementById('start-screen');
 const gamescreen = document.getElementById('game-screen');
 const creditsscreen = document.getElementById('credits-screen');
+const gameoverscreen = document.getElementById('gameover-screen');
 //screen buttons
 const stopBtn = document.getElementById('stop-btn');
 const startBtn = document.getElementById('start-btn');
@@ -15,8 +16,7 @@ startBtn.addEventListener('click',()=>{
     if (typeof timerCountdown === 'function') {
         timerCountdown(GAME_DURATION, ()=>{
             //make a game over screen
-            showScreen('main');
-            console.log("Time is up.");
+            showScreen('gameover');
         }); 
     } else {
         console.error("timerCountdown function did not call");
@@ -39,8 +39,9 @@ function showScreen(screenName){
     mainscreen.style.display = 'none';
     gamescreen.style.display = 'none';
     creditsscreen.style.display = 'none';
-
+    gameoverscreen.style.display = 'none';
     if(screenName === 'main') mainscreen.style.display = 'block'
     if(screenName === 'game') gamescreen.style.display = 'block'
     if(screenName === 'credits') creditsscreen.style.display = 'block'
+    if(screenName === 'gameover') gameoverscreen.style.display = 'block'
 }
